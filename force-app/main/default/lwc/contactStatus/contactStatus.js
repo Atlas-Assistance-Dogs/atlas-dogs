edcc4a82b20be2636531d9a84f361c54c49dc242
Debbie <dengelmeyer@icloud.com>
2021-03-23 22:05:59 -0700

    Remove unused component
    Small corrections on others.  fixes #47

--- force-app/main/default/lwc/contactStatus/contactStatus.js
+++ force-app/main/default/lwc/contactStatus/contactStatus.js
@@ -2,13 +2,13 @@ import { LightningElement, api, track, wire } from "lwc";
 import { getRecord } from "lightning/uiRecordApi";
 
 const FIELDS = [
-    "Contact.Positions__c",
-    "Contact.Volunteer_Status__c",
-    "Contact.Client_Status__c",
-    "Contact.Board_Member_Status__c",
-    "Contact.Facilitator_Status__c",
-    "Contact.Lead_Facilitator_Status__c",
-    "Contact.Trainer_Status__c"
+    "contact.Positions__c",
+    "contact.Volunteer_Status__c",
+    "contact.Client_Status__c",
+    "contact.Board_Member_Status__c",
+    "contact.Facilitator_Status__c",
+    "contact.Lead_Facilitator_Status__c",
+    "contact.Trainer_Status__c"
 ];
 
 export default class ContactStatus extends LightningElement {
@@ -16,24 +16,10 @@ export default class ContactStatus extends LightningElement {
     @track roles = [];
 
     @wire(getRecord, { recordId: "$recordId", fields: FIELDS })
-    Contact;
+    contact;
 
     get role() {
-        if (this.Contact.data.fields.Positions__c.value) {
-            this.roles.push("Volunteer");
-        }
-        if (this.Contact.data.fields.Client__c.value) {
-            this.roles.push("Client");
-        }
-        if (this.Contact.data.fields.Board_Member__c.value) {
-            this.roles.push("Board Memeber");
-        }
-        if (this.Contact.data.fields.Facilitator__c.value) {
-            this.roles.push("Facilitator");
-        }
-        if (this.Contact.data.fields.Trainer__c.value) {
-            this.roles.push("Trainer");
-        }
+        this.roles = this.contact.data.fields.Positions__c.value.split(";");
 
         return this.roles;
     }
