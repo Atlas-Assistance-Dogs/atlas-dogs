3323f74c6e6f32b5b468d6d9157557a066c8e67d
kaurarjinder <58372616+kaurarjinder@users.noreply.github.com>
2021-08-08 19:16:44 -0700

    row actions added to document upload

--- force-app/main/default/lwc/documetUploadCmp/documetUploadCmp.js
+++ force-app/main/default/lwc/documetUploadCmp/documetUploadCmp.js
@@ -2,21 +2,25 @@ import { LightningElement, track, api, wire } from "lwc";
 
 import { ShowToastEvent } from "lightning/platformShowToastEvent";
 import retrieveAllData from "@salesforce/apex/FileController.retrieveAllData";
-import deleteRecords from "@salesforce/apex/FileController.deleteRecords";
+import deleteRecord from "@salesforce/apex/FileController.deleteRecord";
 import updateRecords from "@salesforce/apex/FileController.updateRecords";
 import { refreshApex } from '@salesforce/apex';
+import { NavigationMixin } from 'lightning/navigation';
 
-const actions = [{ label: "Delete", name: "delete" }];
+const actions = [
+    { label: 'Show details', name: 'show_details' },
+    { label: 'Delete', name: 'delete' },
+];
 
 const COLS = [
-    { label: "File Name", fieldName: "Title" },
+    { label: "File Name", fieldName: "Title", type: 'fileUpload',typeAttributes :{acceptedFileFormats: '.jpg,.jpeg,.pdf,.png', recordId: { fieldName: 'Id' }, }},
     { label: "userType", fieldName: "Position_Files__c" },
     { label: "docType", fieldName: "Document_Type__c" },
     { label: "Document Date", fieldName: "CreatedDate", type: "date" },
     { type: "action", typeAttributes: { rowActions: actions }}
 ];
 
-export default class DocumetUploadCmp extends LightningElement {
+export default class DocumetUploadCmp extends NavigationMixin(LightningElement) {
 
     @api objectApiName;
     @api recordId;
@@ -34,9 +38,6 @@ export default class DocumetUploadCmp extends LightningElement {
     errorMessage = "";
     @api fileName;
 
-     // localCountofFiles;
-     // @api countOfFiles;
-
     @wire(retrieveAllData, { recordId: '$recordId' }) filesLst (result) {
         this.wiredFilesList = result;
         if (result.data) {
@@ -49,6 +50,20 @@ export default class DocumetUploadCmp extends LightningElement {
                                     variant: "error"
                                 })
                             );
+        }
+    }
+
+    handleRowAction(event) {
+        const actionName = event.detail.action.name;
+        const row = event.detail.row;
+        switch (actionName) {
+            case 'delete':
+                this.deleteCons(row);
+                break;
+            case 'show_details':
+                this.navigateToFiles(row);
+                break;
+            default:
         }
     }
     
@@ -84,7 +99,9 @@ export default class DocumetUploadCmp extends LightningElement {
         this.updateFiles();
         refreshApex(this.wiredFilesList);
         this.isModalOpen = false;
+        window.location.reload();
     }
+    
     updateFiles() {
         updateRecords( { recordId: this.currenRecordId, position: this.position, docType: this.docType, docName: this.fileName, docId: this.fileID } )
             .then((data) => {
@@ -102,21 +119,36 @@ export default class DocumetUploadCmp extends LightningElement {
             });
     }
 
-    // Getting releated files of the current record
-    /*
-    getRelatedFiles() {
-        retrieveAllData({ recordId: this.currenRecordId  })
-            .then((data) => {
-                this.fileUploadList = data;
-                console.log(
-                    "this.fileUploadList" + JSON.stringify(this.fileUploadList)
+    navigateToFiles(currentRow) {
+        let currentRecordID = currentRow.ContentDocumentId;
+        this[NavigationMixin.Navigate]({
+          type: 'standard__namedPage',
+          attributes: {
+              pageName: 'filePreview'
+          },
+          state : {
+              selectedRecordId:currentRecordID
+          }
+        })
+    }
+
+    deleteCons(currentRow) {
+        let currentRecordID = currentRow.ContentDocumentId;
+        deleteRecord({ docId: currentRecordID })
+            .then((result) => {
+                let res = result;
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: "Success!!",
+                        message: res,
+                        variant: "success"
+                    })
                 );
-                if (this.fileUploadList != null) {
-                    this.localCountofFiles = this.fileUploadList.length;
-                }
-                this.handleErrorOnUpload();
+                this.isErrorMessage = false;
+                refreshApex(this.wiredFilesList);
             })
             .catch((error) => {
+                window.console.log("Error ====> " + error);
                 this.dispatchEvent(
                     new ShowToastEvent({
                         title: "Error!!",
@@ -127,24 +159,4 @@ export default class DocumetUploadCmp extends LightningElement {
             });
     }
 
-    */
-    //Show Error if Count of Files are less
-    /*
-    handleErrorOnUpload() {
-        if (this.localCountofFiles < this.countOfFiles) {
-            this.errorMessage =
-                "Minimum of " + this.countOfFiles + " files are required";
-            this.isErrorMessage = true;
-        } else {
-            this.isErrorMessage = false;
-        }
-    }
-
-     getSelectedRecords(event) {
-
-     }
-
-     */
-
- 
 }
