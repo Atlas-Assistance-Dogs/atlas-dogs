47a4001d6313e34e5d4849eb40bb9a0123254766
Debbie <dengelmeyer@icloud.com>
2022-04-09 17:47:50 -0700

    Clean up relatedBackgroundCheckCmp

--- force-app/main/default/lwc/relatedBackgroundCheckCmp/relatedBackgroundCheckCmp.js
+++ force-app/main/default/lwc/relatedBackgroundCheckCmp/relatedBackgroundCheckCmp.js
@@ -1,15 +1,6 @@
-import { LightningElement, api, wire } from "lwc";
-import { ShowToastEvent } from "lightning/platformShowToastEvent";
-import getRelatedFiles from "@salesforce/apex/FileController.getRelatedFiles";
-import addBackgroundCheck from "@salesforce/apex/BackgroundCheckController.addBackgroundCheck";
-import updateBackgroundCheck from "@salesforce/apex/BackgroundCheckController.updateBackgroundCheck";
+import { LightningElement, api, wire, track } from "lwc";
 import { NavigationMixin } from "lightning/navigation";
 import { refreshApex } from "@salesforce/apex";
-
-import CONTACT_FIELD from "@salesforce/schema/BackgroundCheck__c.Contact__c";
-import DATE_FIELD from "@salesforce/schema/BackgroundCheck__c.Date__c";
-import NOTES_FIELD from "@salesforce/schema/BackgroundCheck__c.Notes__c";
-import RESULT_FIELD from "@salesforce/schema/BackgroundCheck__c.Status__c";
 
 // Import message service features required for subscribing and the message channel
 import {
@@ -24,36 +15,10 @@ export default class RelatedBackgroundCheckCmp extends NavigationMixin(
     LightningElement
 ) {
     @api recordId;
-    @api contactId;
-    currentCv;
-    wiredCv;
+    @api contactId = "";
     message;
     contentType = "Background Check";
     title = "Background Check";
-
-    objectName = CONTACT_FIELD.objectApiName;
-
-    fields = {
-        contact: CONTACT_FIELD,
-        date: DATE_FIELD,
-        notes: NOTES_FIELD,
-        result: RESULT_FIELD
-    };
-
-    get acceptedFormats() {
-        return [
-            ".pdf",
-            ".png",
-            ".jpg",
-            ".jpeg",
-            ".doc",
-            ".docx",
-            ".txt",
-            ".xlsx",
-            ".xls",
-            ".csv"
-        ];
-    }
 
     @api
     openModal() {
@@ -64,107 +29,16 @@ export default class RelatedBackgroundCheckCmp extends NavigationMixin(
         this.template.querySelector("c-modal-cmp").closeModal();
     }
 
-    get hasFile() {
-        return this.currentCv;
-    }
-
-    @wire(getRelatedFiles, { recordId: "$recordId" })
-    getFiles(result) {
-        this.wiredCv = result;
-        this.currentCv = null;
-        if (result.data) {
-            this.currentCv = result.data[0];
-        } else if (result.error) {
-            this.dispatchEvent(
-                new ShowToastEvent({
-                    title: "Error!!",
-                    message: result.error.message,
-                    variant: "error"
-                })
-            );
-        }
-    }
-
-    handleUploadFinished(event) {
-        this.isErrorMessage = false;
-        this.message = "File Uploaded Successfully";
-        this.uploadedFile = event.detail.files[0];
-        this.currentCv = {
-            Title: this.getFileNameWithoutExtension(this.uploadedFile.name),
-            ContentDocumentId: this.uploadedFile.documentId
-        };
-        refreshApex();
-    }
-
-    getFileNameWithoutExtension(name) {
-        let parts = name.split(".");
-        if (parts.length > 1) {
-            return parts.slice(0, -1).join(".");
-        }
-        return name;
-    }
-
     handleSubmit(event) {
         event.preventDefault();
-        let fields = [
-            ...this.template.querySelectorAll("lightning-input-field")
-        ];
-        var record = fields.reduce(
-            (a, x) => ({ ...a, [x.fieldName]: x.value }),
-            {}
-        );
-        record.Contact__c = this.contactId;
-        var documentId = null;
-        if (this.uploadedFile) {
-            documentId = this.uploadedFile.documentId;
-        }
-        if (!this.recordId) {
-            this.createBC(record, documentId);
-        } else {
-            record.Id = this.recordId;
-            this.updateBC(record);
-        }
+        this.template
+            .querySelector("c-background-check-form-cmp")
+            .handleSubmit(event);
     }
 
-    createBC(record, documentId) {
-        addBackgroundCheck({
-            check: record,
-            documentId: documentId
-        })
-            .then((data) => {
-                this.dispatchEvent(new CustomEvent("changed"));
-                this.closeModal();
-            })
-            .catch((error) => {
-                this.dispatchEvent(
-                    new ShowToastEvent({
-                        title: "Error!!",
-                        message: error.body.message,
-                        variant: "error"
-                    })
-                );
-                this.closeModal();
-            });
-    }
-
-    updateBC(record) {
-        updateBackgroundCheck({
-            check: record
-        })
-            .then(() => {
-                this.dispatchEvent(new CustomEvent("changed"));
-                this.closeModal();
-            })
-            .catch((error) => {
-                this.dispatchEvent(
-                    new ShowToastEvent({
-                        title: "Error!!",
-                        message: error.body.message,
-                        variant: "error"
-                    })
-                );
-                this.closeModal();
-            });
+    onChanged(event) {
+        this.dispatchEvent(new CustomEvent("changed"));
+        this.closeModal();
     }
 
     @wire(MessageContext)
@@ -190,7 +64,6 @@ export default class RelatedBackgroundCheckCmp extends NavigationMixin(
     // Handler for message received by component
     handleMessage(message) {
         this.recordId = message.recordId;
-        this.currentCv = null;
         refreshApex();
         this.openModal();
     }
