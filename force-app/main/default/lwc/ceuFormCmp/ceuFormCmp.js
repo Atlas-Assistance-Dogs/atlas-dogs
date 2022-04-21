71cf77185dea5acbaaaad2050eefafbc24184ed8
Deborah Engelmeyer <dengelmeyer@icloud.com>
2022-04-20 18:12:18 -0700

    Bug/ceu form (#186)
    * Create related component for CEUs
    
    Make several fields required
    
    * Modified CEU form to show AuthorityOther field when 'Other'
    
    Made sure list of files is appended to when user uploads files in CEU form
    
    * Remove required field from profile
    
    * Fix adding files
    
    Add status to list
    
    * Swap required from Program Hours to Program Name
    
    Update permission set and profile
    Set default status to Pending
    
    * Correct FileController tests
    
    * Make at least one file required

---
+++ force-app/main/default/lwc/ceuFormCmp/ceuFormCmp.js
@@ -0,0 +1,276 @@
+import { LightningElement, api, wire } from "lwc";
+import { ShowToastEvent } from "lightning/platformShowToastEvent";
+import { NavigationMixin } from "lightning/navigation";
+import createCeu from "@salesforce/apex/ContinuingEducationUnitController.createCeu";
+import updateCeu from "@salesforce/apex/ContinuingEducationUnitController.updateCeu";
+import getRelatedFiles from "@salesforce/apex/FileController.getRelatedFiles";
+import { refreshApex } from "@salesforce/apex";
+
+import AUTHORITY_FIELD from "@salesforce/schema/ContinuingEducationUnit__c.Authority__c";
+import AUTHORITY_OTHER_FIELD from "@salesforce/schema/ContinuingEducationUnit__c.AuthorityOther__c";
+import COMMENTS_FIELD from "@salesforce/schema/ContinuingEducationUnit__c.Comments__c";
+import DATE_COMPLETED_FIELD from "@salesforce/schema/ContinuingEducationUnit__c.DateCompleted__c";
+import DATE_SUBMITTED_FIELD from "@salesforce/schema/ContinuingEducationUnit__c.DateSubmitted__c";
+import PROGRAM_DATE_FIELD from "@salesforce/schema/ContinuingEducationUnit__c.ProgramDate__c";
+import PROGRAM_HOURS_FIELD from "@salesforce/schema/ContinuingEducationUnit__c.ProgramHours__c";
+import PROGRAM_TITLE_FIELD from "@salesforce/schema/ContinuingEducationUnit__c.ProgramTitle__c";
+import QUANTITY_FIELD from "@salesforce/schema/ContinuingEducationUnit__c.Quantity__c";
+import ROLE_FIELD from "@salesforce/schema/ContinuingEducationUnit__c.Role__c";
+import STATUS_FIELD from "@salesforce/schema/ContinuingEducationUnit__c.Status__c";
+import TRAINER_FIELD from "@salesforce/schema/ContinuingEducationUnit__c.Trainer__c";
+
+// Import message service features required for subscribing and the message channel
+import {
+    subscribe,
+    unsubscribe,
+    APPLICATION_SCOPE,
+    MessageContext
+} from "lightning/messageService";
+import ceuForm from "@salesforce/messageChannel/CeuForm__c";
+
+const COLS = [
+    {
+        label: "Files",
+        type: "button",
+        typeAttributes: {
+            name: "view",
+            label: { fieldName: "name" },
+            variant: "base",
+            iconName: "utility:file",
+            iconPosition: "left"
+        }
+    }
+];
+
+export default class CeuFormCmp extends NavigationMixin(LightningElement) {
+    @api contactId;
+    recordId;
+    title = "Continuing Education Unit";
+    mode = "create";
+    relatedFiles = null;
+    wiredFilesList;
+    object = AUTHORITY_FIELD.objectApiName;
+    fields = {
+        authority: AUTHORITY_FIELD,
+        authorityOther: AUTHORITY_OTHER_FIELD,
+        comments: COMMENTS_FIELD,
+        dateCompleted: DATE_COMPLETED_FIELD,
+        dateSubmitted: DATE_SUBMITTED_FIELD,
+        programDate: PROGRAM_DATE_FIELD,
+        programHours: PROGRAM_HOURS_FIELD,
+        programTitle: PROGRAM_TITLE_FIELD,
+        quantity: QUANTITY_FIELD,
+        role: ROLE_FIELD,
+        status: STATUS_FIELD,
+        trainer: TRAINER_FIELD
+    };
+    authority = "Atlas";
+
+    columns = COLS;
+    get acceptedFormats() {
+        return [
+            ".pdf",
+            ".png",
+            ".jpg",
+            ".jpeg",
+            ".doc",
+            ".docx",
+            ".txt",
+            ".xlsx",
+            ".xls",
+            ".zip"
+        ];
+    }
+
+    @api
+    openModal() {
+        this.template.querySelector("c-modal-cmp").openModal();
+    }
+
+    closeModal() {
+        this.template.querySelector("c-modal-cmp").closeModal();
+    }
+
+    handleClose() {}
+
+    get isAuthorityOther() {
+        return this.authority === "Other";
+    }
+
+    handleAuthorityChange(event) {
+        this.authority = event.target.value;
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        const docIds = this.relatedFiles?.map((file) => file.documentId);
+        if (!docIds || docIds.length === 0) {
+            this.dispatchEvent(
+                new ShowToastEvent({
+                    title: "Error!!",
+                    message: "At least one file is required!",
+                    variant: "error"
+                })
+            );
+            return;
+        }
+
+        let fields = [
+            ...this.template.querySelectorAll("lightning-input-field")
+        ];
+        var record = fields.reduce(
+            (a, x) => ({ ...a, [x.fieldName]: x.value }),
+            {}
+        );
+        record.Id = this.recordId;
+        record.Trainer__c = this.contactId;
+        if (this.mode === "create") {
+            this.createNewCeu(record, docIds);
+        } else {
+            this.editCeu(record);
+        }
+    }
+
+    @wire(MessageContext)
+    messageContext;
+
+    // Create a new log
+    createNewCeu(record, docIds) {
+        createCeu({
+            record: record,
+            documentIds: docIds
+        })
+            .then(() => {
+                this.dispatchEvent(new CustomEvent("changed"));
+                this.closeModal();
+            })
+            .catch((error) => {
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: "Error!!",
+                        message: error.body.message,
+                        variant: "error"
+                    })
+                );
+            });
+    }
+
+    // Update an existing PAT
+    editCeu(record) {
+        updateCeu({ record: record })
+            .then(() => {
+                this.dispatchEvent(new CustomEvent("changed"));
+                this.closeModal();
+            })
+            .catch((error) => {
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: "Error!!",
+                        message: error.body.message,
+                        variant: "error"
+                    })
+                );
+            });
+    }
+
+    // Encapsulate logic for Lightning message service subscribe and unsubsubscribe
+    subscribeToMessageChannel() {
+        if (!this.subscription) {
+            this.subscription = subscribe(
+                this.messageContext,
+                ceuForm,
+                (message) => this.handleMessage(message),
+                { scope: APPLICATION_SCOPE }
+            );
+        }
+    }
+
+    unsubscribeToMessageChannel() {
+        unsubscribe(this.subscription);
+        this.subscription = null;
+    }
+
+    // Handler for message received by component
+    handleMessage(message) {
+        this.recordId = message.recordId;
+        if (this.recordId) {
+            refreshApex(this.wiredFilesList);
+        } else {
+            this.relatedFiles = null;
+        }
+        this.mode = message.recordId ? "edit" : "create";
+        this.authority = message.authority;
+        this.openModal();
+    }
+
+    // Standard lifecycle hooks used to subscribe and unsubsubscribe to the message channel
+    connectedCallback() {
+        this.subscribeToMessageChannel();
+    }
+
+    disconnectedCallback() {
+        this.unsubscribeToMessageChannel();
+    }
+
+    get hasFiles() {
+        return this.relatedFiles !== null && this.relatedFiles.length > 0;
+    }
+
+    @wire(getRelatedFiles, { recordId: "$recordId" }) filesLst(result) {
+        this.wiredFilesList = result;
+        this.relatedFiles = null;
+        if (result.data) {
+            this.relatedFiles = result.data.map((data) => {
+                return {
+                    name: data.Title,
+                    documentId: data.ContentDocumentId
+                };
+            });
+        } else if (result.error) {
+            this.relatedFiles = null;
+            this.dispatchEvent(
+                new ShowToastEvent({
+                    title: "Error!!",
+                    message: result.error.message,
+                    variant: "error"
+                })
+            );
+        }
+    }
+
+    handleUpdate() {
+        refreshApex(this.wiredFilesList);
+    }
+
+    handleUploadFinished(event) {
+        if (this.relatedFiles) {
+            this.relatedFiles = this.relatedFiles.concat(event.detail.files);
+        } else {
+            this.relatedFiles = event.detail.files;
+        }
+    }
+
+    handleRowAction(event) {
+        const actionName = event.detail.action.name;
+        const row = event.detail.row;
+        switch (actionName) {
+            case "view":
+                this.navigateToFiles(row);
+                break;
+            default:
+        }
+    }
+
+    navigateToFiles(currentRow) {
+        let currentRecordID = currentRow.documentId;
+        this[NavigationMixin.Navigate]({
+            type: "standard__namedPage",
+            attributes: {
+                pageName: "filePreview"
+            },
+            state: {
+                selectedRecordId: currentRecordID
+            }
+        });
+    }
+}
---
+++ force-app/main/default/lwc/relatedCeusCmp/relatedCeusCmp.js
@@ -0,0 +1,151 @@
+import { LightningElement, api, wire } from "lwc";
+import { ShowToastEvent } from "lightning/platformShowToastEvent";
+import { NavigationMixin } from "lightning/navigation";
+import getRelatedCeus from "@salesforce/apex/ContinuingEducationUnitController.getRelatedCeus";
+import deleteCeu from "@salesforce/apex/ContinuingEducationUnitController.deleteCeu";
+import { refreshApex } from "@salesforce/apex";
+
+// Import message service features required for publishing and the message channel
+import { publish, MessageContext } from "lightning/messageService";
+import ceuForm from "@salesforce/messageChannel/CeuForm__c";
+
+const actions = [
+    { label: "Edit", name: "edit" },
+    { label: "Delete", name: "delete" }
+];
+
+const COLS = [
+    { label: "Authority", fieldName: "authority" },
+    { label: "Program Date", fieldName: "ProgramDate__c", type: "date" },
+    { label: "Date Completed", fieldName: "DateCompleted__c", type: "date" },
+    { label: "Program Hours", fieldName: "ProgramHours__c", type: "number" },
+    { label: "Role", fieldName: "Role__c" },
+    { label: "Status", fieldName: "Status__c" },
+    { type: "action", typeAttributes: { rowActions: actions } }
+];
+
+export default class RelatedCeusCmp extends NavigationMixin(LightningElement) {
+    @api recordId;
+    columns = COLS;
+    data = [];
+    wiredCeus;
+
+    @wire(MessageContext)
+    messageContext;
+
+    defaultSortDirection = "asc";
+    sortDirection = "asc";
+    sortedBy;
+
+    // Used to sort the 'Age' column
+    sortBy(field, reverse, primer) {
+        const key = primer
+            ? function (x) {
+                  return primer(x[field]);
+              }
+            : function (x) {
+                  return x[field];
+              };
+
+        return function (a, b) {
+            a = key(a);
+            b = key(b);
+            return reverse * ((a > b) - (b > a));
+        };
+    }
+
+    handleSort(event) {
+        const { fieldName: sortedBy, sortDirection } = event.detail;
+        const cloneData = [...this.data];
+
+        cloneData.sort(this.sortBy(sortedBy, sortDirection === "asc" ? 1 : -1));
+        this.data = cloneData;
+        this.sortDirection = sortDirection;
+        this.sortedBy = sortedBy;
+    }
+
+    handleRowAction(event) {
+        const actionName = event.detail.action.name;
+        const row = event.detail.row;
+        switch (actionName) {
+            case "delete":
+                this.deleteCeu(row.Id);
+                break;
+            case "edit":
+                const payload = {
+                    authority: row.Authority__c,
+                    recordId: row.Id
+                };
+                publish(this.messageContext, ceuForm, payload);
+                break;
+            case "view":
+                this.navigateToFiles(row);
+                break;
+        }
+    }
+
+    navigateToFiles(currentRow) {
+        let currentRecordID = currentRow.documentId;
+        this[NavigationMixin.Navigate]({
+            type: "standard__namedPage",
+            attributes: {
+                pageName: "filePreview"
+            },
+            state: {
+                selectedRecordId: currentRecordID
+            }
+        });
+    }
+
+    @wire(getRelatedCeus, { contactId: "$recordId" })
+    getCeus(result) {
+        this.wiredCeus = result;
+        this.data = null;
+        if (result.data) {
+            this.data = result.data.map((ceu) => {
+                var newCeu = Object.assign({}, ceu);
+                newCeu["authority"] = ceu.Authority__c;
+                if (newCeu.authority === "Other") {
+                    newCeu.authority = ceu.AuthorityOther__c;
+                }
+                return newCeu;
+            });
+            if (this.data.length === 0) {
+                this.data = null;
+            }
+        } else if (result.error) {
+            this.dispatchEvent(
+                new ShowToastEvent({
+                    title: "Error!!",
+                    message: result.error.message,
+                    variant: "error"
+                })
+            );
+        }
+    }
+
+    createCeu(event) {
+        const payload = { mode: "create" };
+        publish(this.messageContext, ceuForm, payload);
+    }
+
+    deleteCeu(recordId) {
+        deleteCeu({ recordId: recordId })
+            .then(() => {
+                this.handleChange();
+            })
+            .catch((error) => {
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: "Error!!",
+                        message: error.body.message,
+                        variant: "error"
+                    })
+                );
+            });
+    }
+
+    handleChange() {
+        refreshApex(this.wiredCeus);
+    }
+}
