af858017fad13229efc9513193eb8fa0750e851a
Debbie <dengelmeyer@icloud.com>
2022-09-06 20:41:09 -0700

    Add contract received file type for Puppy Raiser
    Add last received field
    Use picklist labels in related files component

--- force-app/main/default/lwc/relatedFilesCmp/relatedFilesCmp.js
+++ force-app/main/default/lwc/relatedFilesCmp/relatedFilesCmp.js
@@ -1,10 +1,13 @@
 import { LightningElement, api, wire, track } from "lwc";
+import { getObjectInfo } from 'lightning/uiObjectInfoApi';
+import { getPicklistValues } from 'lightning/uiObjectInfoApi';
 import getRelatedFiles from "@salesforce/apex/FileController.getRelatedFiles";
 import deleteRecord from "@salesforce/apex/FileController.deleteRecord";
 import { refreshApex } from "@salesforce/apex";
 import { NavigationMixin } from "lightning/navigation";
 import { ShowToastEvent } from "lightning/platformShowToastEvent";
 
+import CV_OBJECT from '@salesforce/schema/ContentVersion';
 import CATEGORY_FIELD from "@salesforce/schema/ContentVersion.Category__c";
 import TYPE_FIELD from "@salesforce/schema/ContentVersion.Type__c";
 import DATE_FIELD from "@salesforce/schema/ContentVersion.Date__c";
@@ -30,8 +33,8 @@ const COLS = [
             iconPosition: "left"
         }
     },
-    { label: "Category", fieldName: CATEGORY_FIELD.fieldApiName },
-    { label: "Document Type", fieldName: TYPE_FIELD.fieldApiName },
+    { label: "Category", fieldName: 'category' },
+    { label: "Document Type", fieldName: 'type' },
     { label: "Document Date", fieldName: DATE_FIELD.fieldApiName, type: "date-local" },
     { type: "action", typeAttributes: { rowActions: actions } }
 ];
@@ -45,12 +48,45 @@ export default class RelatedFiles extends NavigationMixin(LightningElement) {
     openModal() {
         this.template.querySelector("c-file-upload-cmp").openModal();
     }
+    @wire(getObjectInfo, { objectApiName: CV_OBJECT })
+    objectInfo;
+
+    get recordTypeId() {
+        // Returns a map of record type Ids 
+        const rtis = this.objectInfo.data.recordTypeInfos;
+        return Object.keys(rtis).find(rti => rtis[rti].name === 'Special Account');
+    }
+
+    types;
+
+    @wire(getPicklistValues, { recordTypeId: "$objectInfo.data.defaultRecordTypeId", fieldApiName: TYPE_FIELD })
+    getTypeValues(result) {
+        if (result.data && result.data.values && result.data.values.length > 0)
+        {
+            this.types = Object.assign({}, ...result.data.values.map((x) => ({[x.value]: x.label})));
+        }
+    }
+
+    categories;
+
+    @wire(getPicklistValues, { recordTypeId: "$objectInfo.data.defaultRecordTypeId", fieldApiName: CATEGORY_FIELD })
+    getCategoryValues(result) {
+        if (result.data && result.data.values && result.data.values.length > 0)
+        {
+            this.categories = Object.assign({}, ...result.data.values.map((x) => ({[x.value]: x.label})));
+        }
+    }
 
     @wire(getRelatedFiles, { recordId: "$recordId" }) filesLst(result) {
         this.wiredFilesList = result;
         this.fileUploadList = null;
         if (result.data && result.data.length > 0) {
-            this.fileUploadList = result.data;
+            this.fileUploadList = result.data.map(x => {
+                let info = Object.assign({}, x);
+                info['type'] = this.types[x[TYPE_FIELD.fieldApiName]];
+                info['category'] = this.categories[x[CATEGORY_FIELD.fieldApiName]];
+                return info;
+            });
         } else if (result.error) {
             this.dispatchEvent(
                 new ShowToastEvent({
