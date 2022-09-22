1cd539078178078c4398cd82b25163c0a7f5f047
Debbie <dengelmeyer@icloud.com>
2022-09-22 00:14:42 -0700

    Add related items total  and View all to CEUs

--- force-app/main/default/lwc/relatedCeusCmp/relatedCeusCmp.js
+++ force-app/main/default/lwc/relatedCeusCmp/relatedCeusCmp.js
@@ -46,9 +46,11 @@ const COLS = [
 
 export default class RelatedCeusCmp extends NavigationMixin(LightningElement) {
     @api recordId;
+    @api max = 6;
     columns = COLS;
     data = [];
     wiredCeus;
+    total = 0;
 
     @wire(MessageContext)
     messageContext;
@@ -117,12 +119,12 @@ export default class RelatedCeusCmp extends NavigationMixin(LightningElement) {
         });
     }
 
-    @wire(getRelatedCeus, { contactId: "$recordId" })
+    @wire(getRelatedCeus, { contactId: "$recordId", max: "$max" })
     getCeus(result) {
         this.wiredCeus = result;
         this.data = null;
         if (result.data) {
-            this.data = result.data.map((ceu) => {
+            this.data = result.data.items.map((ceu) => {
                 var newCeu = Object.assign({}, ceu);
                 newCeu["authority"] = ceu[AUTHORITY_FIELD.fieldApiName];
                 if (newCeu.authority === "Other") {
@@ -130,9 +132,12 @@ export default class RelatedCeusCmp extends NavigationMixin(LightningElement) {
                 }
                 return newCeu;
             });
-            this.total = this.data.length;
             if (this.data.length === 0) {
                 this.data = null;
+                this.total = 0;
+            }
+            if (this.max < 15) {
+                this.total = result.data.total;
             }
         } else if (result.error) {
             this.dispatchEvent(
@@ -168,5 +173,18 @@ export default class RelatedCeusCmp extends NavigationMixin(LightningElement) {
 
     handleChange() {
         refreshApex(this.wiredCeus);
+    }
+
+    handleViewAll() {
+        // Navigate to a specific component.
+        this[NavigationMixin.Navigate]({
+            type: 'standard__component',
+            attributes: {
+                componentName: 'c__ContinuingEducationUnitsCmp'
+            },
+            state: {
+                c__id: this.recordId
+            }
+        });
     }
 }
