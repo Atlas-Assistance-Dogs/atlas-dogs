e9aa5e3b6484512caa1a3100306e4ccf4ba4e552
Deborah Engelmeyer <dengelmeyer@icloud.com>
2022-10-10 21:03:17 -0700

    Feature/cleanup modals (#350)
    * Remove message channels for remaining related list objects
    
    Background Check, CEU, and Files
    
    * Fix problem on save
    
    * Corrected error with page redirect in BackgroundCheck
    
    Fixed delete for Background Check
    Removed tests for removed methods

--- force-app/main/default/lwc/backgroundCheckFormCmp/backgroundCheckFormCmp.js
+++ force-app/main/default/lwc/backgroundCheckFormCmp/backgroundCheckFormCmp.js
@@ -1,8 +1,7 @@
 import { LightningElement, api, wire } from "lwc";
 import { ShowToastEvent } from "lightning/platformShowToastEvent";
 import getRelatedFiles from "@salesforce/apex/FileController.getRelatedFiles";
-import addBackgroundCheck from "@salesforce/apex/BackgroundCheckController.addBackgroundCheck";
-import updateBackgroundCheck from "@salesforce/apex/BackgroundCheckController.updateBackgroundCheck";
+import relateFile from "@salesforce/apex/FileController.relateFile";
 import { NavigationMixin } from "lightning/navigation";
 import { refreshApex } from "@salesforce/apex";
 
@@ -115,50 +114,31 @@ export default class BackgroundCheckFormCmp extends NavigationMixin(
             );
             return;
         }
-        if (!this.recordId) {
-            this.createBC(record, documentId);
-        } else {
-            record.Id = this.recordId;
-            this.updateBC(record);
+        this.template
+            .querySelector("lightning-record-edit-form")
+            .submit(record);
+    }
+
+    handleSuccess(event) {
+        if (!this.recordId) { // new background check
+            relateFile({documentId: this.currentCv.ContentDocumentId, recordId: event.detail.id})
+                .then((id) => {
+                    this.dispatchEvent(new CustomEvent("changed", { detail: id }));
+                })
+                .catch((error) => {
+                    this.dispatchEvent(
+                        new ShowToastEvent({
+                            title: "Error!!",
+                            message: error.body.message,
+                            variant: "error"
+                        })
+                    );
+                });
         }
-    }
-
-    createBC(record, documentId) {
-        addBackgroundCheck({
-            check: record,
-            documentId: documentId
-        })
-            .then((id) => {
-                this.dispatchEvent(new CustomEvent("changed", { detail: id }));
-            })
-            .catch((error) => {
-                this.dispatchEvent(
-                    new ShowToastEvent({
-                        title: "Error!!",
-                        message: error.body.message,
-                        variant: "error"
-                    })
-                );
-            });
-    }
-
-    updateBC(record) {
-        updateBackgroundCheck({
-            check: record
-        })
-            .then(() => {
-                this.dispatchEvent(
-                    new CustomEvent("changed", { detail: this.recordId })
-                );
-            })
-            .catch((error) => {
-                this.dispatchEvent(
-                    new ShowToastEvent({
-                        title: "Error!!",
-                        message: error.body.message,
-                        variant: "error"
-                    })
-                );
-            });
+        else {
+            this.dispatchEvent(
+                new CustomEvent("changed")
+            );
+        }
     }
 }
