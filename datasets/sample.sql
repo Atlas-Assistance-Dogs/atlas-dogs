7ff25cdcf278d580816ec51115b0a71903abc654
Debbie <dengelmeyer@icloud.com>
2023-03-26 09:15:54 -0700

    Fix sample data

--- datasets/sample.sql
+++ datasets/sample.sql
@@ -300,15 +300,15 @@ INSERT INTO "CategoryRule__c" VALUES(1,'Dog','Dog','');
 INSERT INTO "CategoryRule__c" VALUES(2,'Client','Team','');
 INSERT INTO "CategoryRule__c" VALUES(3,'Dog','Team','');
 INSERT INTO "CategoryRule__c" VALUES(4,'ADSiM','Contact','');
-INSERT INTO "CategoryRule__c" VALUES(4,'Standalone','Contact','');
-INSERT INTO "CategoryRule__c" VALUES(5,'Board','Contact','Board Member');
-INSERT INTO "CategoryRule__c" VALUES(6,'Client','Contact','Client');
-INSERT INTO "CategoryRule__c" VALUES(7,'Facilitator','Contact','Team Facilitator');
-INSERT INTO "CategoryRule__c" VALUES(8,'Facilitator','Contact','Team Facilitator Lead');
-INSERT INTO "CategoryRule__c" VALUES(9,'Puppy','Contact','Puppy Raiser');
-INSERT INTO "CategoryRule__c" VALUES(10,'Staff','Contact','Staff');
-INSERT INTO "CategoryRule__c" VALUES(11,'Trainer','Contact','Trainer');
-INSERT INTO "CategoryRule__c" VALUES(12,'Volunteer','Contact','Volunteer');
+INSERT INTO "CategoryRule__c" VALUES(5,'Standalone','Contact','');
+INSERT INTO "CategoryRule__c" VALUES(6,'Board','Contact','Board Member');
+INSERT INTO "CategoryRule__c" VALUES(7,'Client','Contact','Client');
+INSERT INTO "CategoryRule__c" VALUES(8,'Facilitator','Contact','Team Facilitator');
+INSERT INTO "CategoryRule__c" VALUES(9,'Facilitator','Contact','Team Facilitator Lead');
+INSERT INTO "CategoryRule__c" VALUES(10,'Puppy','Contact','Puppy Raiser');
+INSERT INTO "CategoryRule__c" VALUES(11,'Staff','Contact','Staff');
+INSERT INTO "CategoryRule__c" VALUES(12,'Trainer','Contact','Trainer');
+INSERT INTO "CategoryRule__c" VALUES(13,'Volunteer','Contact','Volunteer');
 CREATE TABLE "Contact" (
 	id INTEGER NOT NULL, 
 	"ADSiMApplicationReceived__c" VARCHAR(255), 
