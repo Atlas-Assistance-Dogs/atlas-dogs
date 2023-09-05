5a8d0bbf5fcc06de0357677fb45725cd011f2ab1
Debbie <dengelmeyer@icloud.com>
2023-09-05 08:33:34 -0700

    Create permission set and group for Volunteer
    add logic to CategoryRuleController to check the group
    add logic to relatedObject to check permissions

--- force-app/main/default/lwc/relatedObjectCmp/relatedObjectCmp.js
+++ force-app/main/default/lwc/relatedObjectCmp/relatedObjectCmp.js
@@ -1,12 +1,11 @@
-import { LightningElement, wire, api } from "lwc";
+import { LightningElement, wire, api, track } from "lwc";
 import { NavigationMixin } from "lightning/navigation";
+import { getObjectInfo } from "lightning/uiObjectInfoApi";
 import { getRecord } from "lightning/uiRecordApi";
 import DOG_OBJECT from "@salesforce/schema/Dog__c";
 
 const MAX_DISPLAY = 6;
-export default class RelatedObjectCmp extends NavigationMixin(
-    LightningElement
-) {
+export default class RelatedObjectCmp extends NavigationMixin(LightningElement) {
     @api viewAll = false;
     @api iconName = "standard:folder";
     @api title = "Related Objects";
@@ -15,6 +14,15 @@ export default class RelatedObjectCmp extends NavigationMixin(
     @api recordId;
     @api auraCompName;
     @api objectApiName;
+
+    @track objectInfo;
+
+    @wire(getObjectInfo, { objectApiName: this.objectApiName })
+    objectInfo;
+
+    get visible() {
+        return this.objectInfo.data.queryable;
+    }
 
     get showBreadCrumbs() {
         return this.viewAll && this.recordId && this.recordId.trim();
