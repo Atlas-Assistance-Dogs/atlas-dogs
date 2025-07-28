026572210a173a7f316e1e5b5e70e93d4283ce29
Debbie <dengelmeyer@icloud.com>
2025-07-27 17:56:28 -0700

    fun with salesforce api

--- force-app/main/default/lwc/__tests__/flushPromises.js
+++
@@ -1,7 +0,0 @@
-  // Helper function to wait until the microtask queue is empty. This is needed for promise
-  // timing when calling imperative Apex.
-  async function flushPromises() {
-    return Promise.resolve();
-  }
-
-  export default flushPromises
