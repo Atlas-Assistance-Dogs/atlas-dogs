cdd07167ebecd27790a08eb9f924616cf0685866
Debbie <dengelmeyer@icloud.com>
2026-06-06 15:12:32 -0700

    Add -Active to other Certifieds

--- force-app/main/default/lwc/contactStatusIndicatorCmp/contactStatusIndicatorCmp.js
+++ force-app/main/default/lwc/contactStatusIndicatorCmp/contactStatusIndicatorCmp.js
@@ -24,7 +24,7 @@ export default class ContactStatusIndicatorCmp extends LightningElement {
         return "custom:custom82";
       case "Action Needed":
         return "standard:actions_and_buttons";
-      case "Certified-Hold":
+      case "Certified-OnHold":
       case "Inactive":
       case "On Hold":
       case "Suspended":
